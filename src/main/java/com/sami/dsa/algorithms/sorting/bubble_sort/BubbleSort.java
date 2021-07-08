d971a8080dc12927c5302cc278e8f53f5482480f
z00450vb <sami.jradi@siemens.com>
2021-07-08 19:07:30 +0200

    implements BubbleSort

---
+++ src/main/java/com/sami/dsa/algorithms/sorting/bubble_sort/BubbleSort.java
@@ -0,0 +1,33 @@
+package com.sami.dsa.algorithms.sorting.bubble_sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public void bubbleSort(int[] array) {
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+
+            for (int j = 0; j < length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // swap numbers
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BubbleSort bs = new BubbleSort();
+
+        int[] array = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+
+        System.out.println(Arrays.toString(array));
+
+        bs.bubbleSort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
