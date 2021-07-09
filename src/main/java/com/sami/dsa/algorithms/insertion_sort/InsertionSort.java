8eda4aa6712cfe6b9cb4a0c82c57b8f7f52fb038
z00450vb <sami.jradi@siemens.com>
2021-07-09 12:21:49 +0200

    implements InsertionSort

---
+++ src/main/java/com/sami/dsa/algorithms/insertion_sort/InsertionSort.java
@@ -0,0 +1,40 @@
+package com.sami.dsa.algorithms.insertion_sort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public void insertionSort(int[] array) {
+
+        int length = array.length;
+
+        for (int i = 0; i < length; i++) {
+
+            int smallest = i;
+
+            for (int j = i; j < length; j++) {
+                if (array[j] < array[smallest]) {
+                    smallest = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[smallest];
+            array[smallest] = temp;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        InsertionSort is = new InsertionSort();
+
+        int[] array = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+
+        System.out.println(Arrays.toString(array));
+
+        is.insertionSort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
